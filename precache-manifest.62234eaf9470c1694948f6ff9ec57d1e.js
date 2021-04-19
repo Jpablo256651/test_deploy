b7a5b94346b6a39f165bc4419ce70fae8cd99316
Jpablo256651 <l161080140@iztapalapa.tecnm.mx>
2021-04-18 19:04:10 -0500

    Updates

---
+++ precache-manifest.62234eaf9470c1694948f6ff9ec57d1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec9833a202224a04fa0bb25e007b424",
+    "url": "/test_deploy/index.html"
+  },
+  {
+    "revision": "2fd1233b150462b39cf5",
+    "url": "/test_deploy/static/css/main.8a15d8e6.chunk.css"
+  },
+  {
+    "revision": "d397f2b46bef4d33c7ba",
+    "url": "/test_deploy/static/js/2.6535edd6.chunk.js"
+  },
+  {
+    "revision": "2fd1233b150462b39cf5",
+    "url": "/test_deploy/static/js/main.b1b71c6a.chunk.js"
+  },
+  {
+    "revision": "bc3da7e283ccccc98305",
+    "url": "/test_deploy/static/js/runtime~main.c33c1e06.js"
+  }
+]);
